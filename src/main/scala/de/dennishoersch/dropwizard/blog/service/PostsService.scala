66b5e8401f6a53e776d8e03f0f54e26fc291b6a2
Dennis Hörsch <dennishoersch@web.de>
2014-09-07 20:32:51 +0200

    Seq statt List verwenden

--- src/main/scala/de/dennishoersch/dropwizard/blog/service/PostsService.scala
+++ src/main/scala/de/dennishoersch/dropwizard/blog/service/PostsService.scala
@@ -12,19 +12,25 @@ class PostsService(implicit val db: DB) {
 
   private def posts = db.allPosts()
 
-  def findAll(): List[Post] = posts
+  def findAll(): Seq[Post] = posts
 
   def findById(id: Long): Option[Post] = posts.find(_.id == id)
 
-  def findByAuthor(id: Long): List[Post] = posts.filter(_.author.id == id)
+  def findByAuthor(id: Long): Seq[Post] = posts.filter(_.author.id == id)
 
-  def findByCategory(name: String): List[Post] = posts.filter(_.categories.exists(_.toString == name))
+  def findByCategory(name: String): Seq[Post] = posts.filter(_.categories.exists(_.toString == name))
 
   def findAllAuthors(): Set[Author] = posts.map(_.author).toSet
 
   def findAllCategories(): Set[Category] = posts.flatMap(_.categories).toSet
 
-  def createPost(author: Author, title: String, content: String): Post = {
-    db.savePost(Post(db.nextId, author, LocalDateTime.now, title, content, List(Uncategorized)))
+  
+  def createPost(author: Author, title: String, content: String): Post = createPost(author, title, content, Uncategorized)
+  
+  def createPost(author: Author, title: String, content: String, category: Category): Post = createPost(author, title, content, Seq(category))
+  
+  def createPost(author: Author, title: String, content: String, categories: Seq[Category]): Post = {
+    db.savePost(Post(db.nextId, author, LocalDateTime.now, title, content, categories))
   }
+  
 }
--- src/main/scala/de/dennishoersch/dropwizard/blog/view/controller/PostsController.scala
+++ src/main/scala/de/dennishoersch/dropwizard/blog/view/controller/PostsController.scala
@@ -1,32 +1,22 @@
 package de.dennishoersch.dropwizard.blog.view.controller
 
-import java.util.{ Map => JavaMap, Collection => JavaCollection }
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.PathParam
-import javax.ws.rs.Produces
+import javax.ws.rs.{Consumes, GET, POST, Path, PathParam, Produces}
 import javax.ws.rs.core.MediaType
-import scala.collection.JavaConversions._
-import io.dropwizard.auth.Auth
-import de.dennishoersch.dropwizard.blog.domain.Account
-import de.dennishoersch.dropwizard.blog.domain.Author
+
 import de.dennishoersch.dropwizard.blog.domain.Category.Category
-import de.dennishoersch.dropwizard.blog.domain.Post
+import de.dennishoersch.dropwizard.blog.domain.{Account, Author, Post}
 import de.dennishoersch.dropwizard.blog.service.PostsService
 import de.dennishoersch.util.dropwizard.views.thymeleaf.ThymeleafView
-import de.dennishoersch.util.resources._
-import javax.ws.rs.POST
-import javax.ws.rs.Consumes
-import javax.ws.rs.FormParam
-import javax.ws.rs.core.UriInfo
-import javax.ws.rs.core.Context
-import de.dennishoersch.util.resources.Response
+import io.dropwizard.auth.Auth
+
+import scala.collection.JavaConversions._
 
 @Path("/posts")
 @Produces(Array(MediaType.TEXT_HTML))
 class PostsController(implicit val postsService: PostsService) {
 
-  // Image Urls koennten bspw auch ueber einen controller/resource geladen werden statt assets, wenn sie aus der DB oder so kaemen.
+  // Image Urls koennten bspw auch ueber einen controller/resource geladen werden statt assets,
+  // wenn sie aus der DB oder so kaemen.
 
   @GET
   def all() = new PostsView(postsService.findAll)
@@ -60,26 +50,33 @@ class PostsController(implicit val postsService: PostsService) {
 
   @POST
   @Consumes(Array(MediaType.APPLICATION_FORM_URLENCODED))
-  def createNew(@Auth account: Account, @FormParam("title") title: String, @FormParam("content") content: String, @Context uriInfo: UriInfo) = {
+  def createNew(@Auth account: Account,
+                @FormParam("title") title: String,
+                @FormParam("content") content: String,
+                @FormParam("categories") category: String,
+                @Context uriInfo: UriInfo) = {
+
     // TODO: Let the user select the categories
     // TODO: @BeanParam is part of JAX-RS 2, that might be part of Dropwizard 0.8
 
-    val post = postsService.createPost(account.author, title, content)
+    // TODO Scala Seq erlauben wie Option 
+
+    val post = postsService.createPost(account.author, title, content, Category.withName(category))
     Response.redirectGet(uriInfo, this, post.id)
   }
 
-  class PostsByAuthorView(val authorName: String, posts: List[Post]) extends PostsView(posts) {
+  class PostsByAuthorView(val authorName: String, posts: Seq[Post]) extends PostsView(posts) {
     val authorView = true
   }
 
-  class PostsByCategoryView(val categoryName: String, posts: List[Post]) extends PostsView(posts) {
+  class PostsByCategoryView(val categoryName: String, posts: Seq[Post]) extends PostsView(posts) {
     val categoryView = true
   }
 
-  class PostsView(private val _posts: List[Post])(implicit protected val postsService: PostsService)
+  class PostsView(private val _posts: Seq[Post])(implicit protected val postsService: PostsService)
     extends ThymeleafView("posts")
     with LayoutView {
-    
+
     val posts: JavaCollection[Post] = _posts
 
     val categoriesOfPost: JavaMap[Long, JavaCollection[Category]] = {
@@ -90,5 +87,9 @@ class PostsController(implicit val postsService: PostsService) {
 
   class CreateNewPostView(private val author: Author)(implicit protected val postsService: PostsService)
     extends ThymeleafView("create-new-post")
-    with LayoutView
+    with LayoutView {
+
+    val categories: JavaCollection[Category] = postsService.findAllCategories
+  }
+
 }
