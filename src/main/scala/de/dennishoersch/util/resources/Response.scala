609c4835a6d1e106674dee0e12eb74a388ad0315
Dennis Hörsch <dennishoersch@web.de>
2014-11-09 11:12:10 +0100

    format

--- src/main/scala/de/dennishoersch/util/resources/Response.scala
+++ src/main/scala/de/dennishoersch/util/resources/Response.scala
@@ -1,11 +1,10 @@
 package de.dennishoersch.util.resources
 
-import javax.ws.rs.core.{Response => RSResponse}
-import javax.ws.rs.core.UriInfo
+import javax.ws.rs.core.{UriInfo, Response => RSResponse}
 
 object Response {
-  def redirectGet(uriInfo: UriInfo, mainResource: AnyRef, subpath: Any)
-  	= RSResponse.seeOther(
-  	    uriInfo.getBaseUriBuilder().path(mainResource.getClass).path(subpath.toString).build()
-  	  ).build()
+  def redirectGet(uriInfo: UriInfo, mainResource: AnyRef, subpath: Any) =
+    RSResponse.seeOther(
+      uriInfo.getBaseUriBuilder().path(mainResource.getClass).path(subpath.toString).build()
+    ).build()
 }
